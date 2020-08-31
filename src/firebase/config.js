8655b441b3a1fede4a426544c3c7c26632aba5b0
Siddhant <siddhant@Siddhant-Chaudhari.local>
2020-08-31 21:40:43 +0530

    Deploy: React with Firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDR99Qo_VH4M9TmaiTJq1UEDWG60ZfjfOk",
-  authDomain: "ninja-firegram-1d0fd.firebaseapp.com",
-  databaseURL: "https://ninja-firegram-1d0fd.firebaseio.com",
-  projectId: "ninja-firegram-1d0fd",
-  storageBucket: "ninja-firegram-1d0fd.appspot.com",
-  messagingSenderId: "508624232559",
-  appId: "1:508624232559:web:4ffbda15bbd2342cd66d9a",
+  apiKey: "AIzaSyCPMmqRj7B542cLPX1Zi-D7P-TyyrIxGMM",
+  authDomain: "sid-firegram-8db85.firebaseapp.com",
+  databaseURL: "https://sid-firegram-8db85.firebaseio.com",
+  projectId: "sid-firegram-8db85",
+  storageBucket: "sid-firegram-8db85.appspot.com",
+  messagingSenderId: "549461671921",
+  appId: "1:549461671921:web:801bbb6fd2aa2e3a16ce50",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
